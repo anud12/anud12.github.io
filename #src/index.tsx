785c3be50e502c0f2a28799ff53ba06b5a31245d
alexandru.crihan <alexandru.crihan@esolutions.ro>
2023-06-27 15:50:15 +0300

    commit

--- #src/index.tsx
+++ #src/index.tsx
@@ -4,5 +4,6 @@ import { Link } from "anud12.github.io_ui_base/src/components/atoms/Link";
 module.exports = (
     <Page title="anud12.github.io">
         <Link href={"ui_base"}>ui_base</Link>
+        <Link href={"boxes"}>Boxes</Link>
     </Page>
 )
