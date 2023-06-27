3b9883ac309b5a619987473889dde946986cde8e
alexandru.crihan <alexandru.crihan@esolutions.ro>
2023-06-27 15:40:31 +0300

    changed homepage

---
+++ #src/build.js
@@ -0,0 +1,1 @@
+require("anud12.github.io_ui_compiler").JSX(`${__dirname}/index.tsx`, `${__dirname}/../index.html`);
---
+++ #src/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Page } from "anud12.github.io_ui_base/src/components/Page"
+import { Link } from "anud12.github.io_ui_base/src/components/atoms/Link";
+module.exports = (
+    <Page title="anud12.github.io">
+        <Link href={"ui_base"}>ui_base</Link>
+    </Page>
+)
