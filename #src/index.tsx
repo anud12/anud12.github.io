59d94dc50cd8bee9eb7dc2448666f292baa1a107
anud12 <supstanta_andu@yahoo.com>
2023-07-23 01:54:16 +0000

    commit

--- #src/index.tsx
+++ #src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Page } from "anud12.github.io_ui_base/src/components/Page"
 import { Link } from "anud12.github.io_ui_base/src/components/atoms/Link";
-module.exports = (
+export default (
     <Page title="anud12.github.io">
         <Link href={"ui_base"}>ui_base</Link>
         <Link href={"boxes"}>Boxes</Link>
