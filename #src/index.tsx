cb0929db0e4b224fad6ec8ec87fb2721821cfa5e
alexandru.crihan <alexandru.crihan@esolutions.ro>
2023-08-29 15:33:25 +0300

    commit

--- #src/index.tsx
+++ #src/index.tsx
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { Fragment, useEffect } from "react";
 import { Page } from "anud12.github.io_ui_base/src/components/Page"
 import { Link } from "anud12.github.io_ui_base/src/components/atoms/Link";
+
+const OnLoad = () => {
+  useEffect(() => {
+    if (window) {
+      console.log(window.location)
+    }
+  }, []);
+
+  return <Fragment />
+}
+
 export default (
-    <Page title="anud12.github.io">
-        <Link href={"ui_base"}>ui_base</Link>
-        <Link href={"boxes"}>Boxes</Link>
-    </Page>
+  <Page title="anud12.github.io">
+    <OnLoad />
+    <Link href={"wifi"}>wifi</Link>
+    <Link href={"ui_base"}>ui_base</Link>
+    <Link href={"boxes"}>Boxes</Link>
+  </Page>
 )
